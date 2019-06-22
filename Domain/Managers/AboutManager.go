d33572bc8b2ecb0931f42229763f1671d573920a
sliganslogan <alistairfink@gmail.com>
2019-06-22 05:21:49 -0400

    about manager

--- Domain/Managers/AboutManager.go
+++ Domain/Managers/AboutManager.go
@@ -1,8 +1,9 @@
 package Managers
 
 import (
+	"github.com/google/uuid"
 	"github.com/alistairfink/AlistairFink-Website-BackEnd/Data/Commands"
-	// "github.com/alistairfink/AlistairFink-Website-BackEnd/Domain/DomainModels"
+	"github.com/alistairfink/AlistairFink-Website-BackEnd/Domain/DomainModels"
 )
 
 type AboutManager struct {
@@ -10,10 +11,34 @@ type AboutManager struct {
 	AboutDescriptionCommand *Commands.AboutDescriptionCommand
 }
 
-// func (this *AboutManager) Get(uuid uuid.UUID) (*DomainModels.AboutDomainModel){
+func (this *AboutManager) Get(uuid uuid.UUID) (*DomainModels.AboutDomainModel){
+	about := this.AboutCommand.Get(uuid)
+	if about == nil {
+		return nil
+	}
 
-// }
+	aboutDesc := this.AboutDescriptionCommand.GetByAboutUuid(uuid)
 
-// func (this *AboutManager) Update(model *DomainModels.AboutDomainModel) (*DomainModels.AboutDomainModel) {
+	var domainModel DomainModels.AboutDomainModel
+	domainModel.ToDomainModel(about, aboutDesc)
+	return &domainModel
+}
 
-// }
+func (this *AboutManager) Update(model *DomainModels.AboutDomainModel) (*DomainModels.AboutDomainModel) {
+	if !this.AboutCommand.Exists(model.Uuid) {
+		return nil
+	}
+
+	for _, desc := range *model.Description {
+		if !this.AboutDescriptionCommand.Exists(desc.Uuid) {
+			return nil
+		}
+	}
+
+	this.AboutCommand.Upsert(model.ToDataModel())
+	for _, desc := range *model.Description {
+		this.AboutDescriptionCommand.Upsert(&desc)
+	}
+
+	return this.Get(model.Uuid)
+}
